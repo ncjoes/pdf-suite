b5d57c44363b9f67dd479aa33da818ac5b0590d0
Chukwuemeka Nwobodo <jc.nwobodo@gmail.com>
2016-10-14 16:58:33 +0100

    Add filesystem exception classes

---
+++ src/Exceptions/FileNoFoundException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PDF-Suite
+ *
+ * Author:  Chukwuemeka Nwobodo (jcnwobodo@gmail.com)
+ * Date:    10/14/2016
+ * Time:    2:50 PM
+ **/
+
+namespace NcJoes\PdfSuite\Exceptions;
+
+use NcJoes\PhpPoppler\Exceptions\FileNotFoundException as BaseException;
+
+class FileNotFoundException extends BaseException
+{
+}
---
+++ src/Exceptions/InvalidDirectoryException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PDF-Suite
+ *
+ * Author:  Chukwuemeka Nwobodo (jcnwobodo@gmail.com)
+ * Date:    10/14/2016
+ * Time:    2:53 PM
+ **/
+
+namespace NcJoes\PdfSuite\Exceptions;
+
+use NcJoes\PhpPoppler\Exceptions\InvalidDirectoryException as BaseException;
+
+class InvalidDirectoryException extends BaseException
+{
+
+}
