5d41e3a3e96c95e484b83a8207b089375719fd6f
Chukwuemeka Nwobodo <jc.nwobodo@gmail.com>
2016-10-14 17:00:01 +0100

    Extend NcJoes\PhpPoppler\Config class in NcJoes\PdfSuite\Config

---
+++ src/Config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * PDF-Suite
+ *
+ * Author:  Chukwuemeka Nwobodo (jcnwobodo@gmail.com)
+ * Date:    10/14/2016
+ * Time:    1:41 PM
+ **/
+
+namespace NcJoes\PdfSuite;
+
+use NcJoes\PhpPoppler\Config as BaseConfig;
+
+class Config extends BaseConfig
+{
+
+}
