4ca724771279a092aa7997cf9834595e86890b82
Chukwuemeka Nwobodo <jc.nwobodo@gmail.com>
2016-10-16 14:42:50 +0100

    Integrate Symfony Dom Crawler component into HtmlFile class for Dom manipulation and processing

--- src/PdfSuite/Files/HtmlFile.php
+++ src/PdfSuite/Files/HtmlFile.php
@@ -10,8 +10,38 @@
 namespace NcJoes\PdfSuite\Files;
 
 use NcJoes\PdfSuite\File;
+use Symfony\Component\DomCrawler\Crawler as DOM;
 
 class HtmlFile extends File
 {
+    private $dom;
+
+    public function __construct($path)
+    {
+        parent::__construct($path);
+        $this->put(str_replace(["Â"], "", $this->get()));
+
+        $dom = new DOM($this->get());
+        $this->dom = $dom;
+    }
+
+    public function getDOM()
+    {
+        $this->dom->clear();
+        $this->dom->addHtmlContent($this->get());
+
+        return $this->dom;
+    }
+
+    public function putDOM(DOM $dom, $save = false)
+    {
+        return $this->put($dom->html(), $save);
+    }
+
+    public function __destruct()
+    {
+        parent::__destruct();
+        unset($this->dom);
+    }
 
 }
