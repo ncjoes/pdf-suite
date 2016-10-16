39015b59e2928b53bf972c0572db960613668a8a
Chukwuemeka Nwobodo <jc.nwobodo@gmail.com>
2016-10-16 04:01:23 +0100

    Simplify exception classes

---
+++ src/PdfSuite/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * pdf-suite
+ *
+ * Author:  Chukwuemeka Nwobodo (jcnwobodo@gmail.com)
+ * Date:    10/15/2016
+ * Time:    6:12 PM
+ **/
+
+namespace NcJoes\PdfSuite;
+
+class Exception extends \Exception
+{
+
+}
