3d3a106374e5dd153897787fb92cb76a6c663367
Chukwuemeka Nwobodo <jc.nwobodo@gmail.com>
2016-10-16 04:09:41 +0100

    PDF to SVG Converter

---
+++ src/PdfSuite/Utils/PdfToSvg.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * PDF-Suite
+ *
+ * Author:  Chukwuemeka Nwobodo (jcnwobodo@gmail.com)
+ * Date:    10/14/2016
+ * Time:    7:52 PM
+ **/
+
+namespace NcJoes\PdfSuite\Utils;
+
+use NcJoes\PdfSuite\Directory;
+use NcJoes\PdfSuite\Files\SvgFile;
+use NcJoes\PopplerPhp\Constants as C;
+use NcJoes\PopplerPhp\PdfToCairo as PdfToCairoUtil;
+
+class PdfToSvg extends PdfToCairo
+{
+    /**
+     * @return Directory
+     */
+    public function convert()
+    {
+        /**
+         * @var $util PdfToCairoUtil
+         */
+        $util = $this->util;
+
+        $util->setOutputSubDir(uniqid());
+        $directory = new Directory($util->getOutputPath(), true);
+
+        $num_pages = $this->pdfInfo()->getNumOfPages();
+        for ($page = 1; $page <= $num_pages; $page++) {
+            $number = str_pad($page, strlen((string)$num_pages), '0', STR_PAD_LEFT);
+            $util->setOutputFilenamePrefix('page-'.$number);
+            $util->startFromPage($page);
+            $util->stopAtPage($page);
+            $util->generateSVG();
+
+            $path = $util->getOutputPath().C::DS.$util->getOutputFilenamePrefix().$util->outputExtension();
+            $directory->addFile(new SvgFile($path));
+        }
+
+        return $directory;
+    }
+}
