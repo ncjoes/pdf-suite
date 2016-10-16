6df064dfb5fc42f2b32384d23863a23cf77fb2b0
Chukwuemeka Nwobodo <jc.nwobodo@gmail.com>
2016-10-16 04:08:43 +0100

    PDF to PNG Converter

---
+++ src/PdfSuite/Utils/PdfToPng.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * PDF-Suite
+ *
+ * Author:  Chukwuemeka Nwobodo (jcnwobodo@gmail.com)
+ * Date:    10/14/2016
+ * Time:    7:50 PM
+ **/
+
+namespace NcJoes\PdfSuite\Utils;
+
+use Illuminate\Filesystem\Filesystem;
+use NcJoes\PdfSuite\Directory;
+use NcJoes\PdfSuite\Files\PngFile;
+use NcJoes\PopplerPhp\Helpers;
+use NcJoes\PopplerPhp\PdfToCairo as PdfToCairoUtil;
+
+class PdfToPng extends PdfToCairo
+{
+    /**
+     * @return Directory
+     */
+    public function convert()
+    {
+        /**
+         * @var $util PdfToCairoUtil
+         */
+        $util = $this->util;
+
+        $util->setOutputSubDir(uniqid());
+        $directory = new Directory($util->getOutputPath(), true);
+
+        $util->setOutputFilenamePrefix('page');
+        $util->generateJPG();
+
+        $fileSystem = new Filesystem;
+        $files = $fileSystem->files($directory->path());
+
+        foreach ($files as $file) {
+            $file = Helpers::parseFileRealPath($file);
+            $directory->addFile(new PngFile($file));
+        }
+
+        return $directory;
+    }
+}
