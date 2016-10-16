b70f4a453c38509f72a754c154159d14f4d5239c
Chukwuemeka Nwobodo <jc.nwobodo@gmail.com>
2016-10-16 04:07:06 +0100

    Update poppler-php PdfToCairo wrapper

--- src/PdfSuite/Utils/PdfToCairo.php
+++ src/PdfSuite/Utils/PdfToCairo.php
@@ -7,16 +7,20 @@
  * Time:    7:08 PM
  **/
 
-namespace NcJoes\PdfSuite\Wrappers;
+namespace NcJoes\PdfSuite\Utils;
 
-use NcJoes\PhpPoppler\PdfToCairo as PdfToCairoUtil;
+use NcJoes\PdfSuite\Directory;
+use NcJoes\PopplerPhp\PdfToCairo as PdfToCairoUtil;
 
-abstract class PdfToCairo extends PhpPopplerUtil
+abstract class PdfToCairo extends PopplerUtil
 {
     public function __construct(PdfToCairoUtil $util)
     {
         return parent::__construct($util);
     }
 
+    /**
+     * @return Directory
+     */
     abstract public function convert();
 }
